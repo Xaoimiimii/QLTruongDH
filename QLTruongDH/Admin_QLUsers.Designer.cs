aa253fd62cfd2859772ae777eddea6ae072cea1c
Nhu <gaococonut1199@gmail.com>
2025-04-17 04:23:00 +0700

    Add PhanHe1 QLUser UI

--- QLTruongDH/Admin_QLUsers.Designer.cs
+++ QLTruongDH/Admin_QLUsers.Designer.cs
@@ -28,8 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
             label1 = new Label();
+            dataGridView = new DataGridView();
+            DBA_SYS_PRIVS_dataGridView = new DataGridView();
+            DBA_TAB_PRIVS_dataGridView = new DataGridView();
+            DBA_ROLE_PRIVS_dataGridView = new DataGridView();
+            delete_button = new Button();
+            edit_button = new Button();
+            search_employee_button = new Button();
+            add_button = new Button();
+            search_employee_guna2TextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            search_employee_label = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DBA_SYS_PRIVS_dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DBA_TAB_PRIVS_dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DBA_ROLE_PRIVS_dataGridView).BeginInit();
             SuspendLayout();
             // 
             // guna2Separator1
@@ -52,16 +68,174 @@
             label1.TabIndex = 13;
             label1.Text = "Quản lý users";
             // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.BackgroundColor = Color.White;
+            dataGridView.BorderStyle = BorderStyle.Fixed3D;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.GridColor = Color.BurlyWood;
+            dataGridView.Location = new Point(15, 212);
+            dataGridView.Name = "dataGridView";
+            dataGridView.ReadOnly = true;
+            dataGridView.RowHeadersWidth = 82;
+            dataGridView.Size = new Size(979, 824);
+            dataGridView.TabIndex = 15;
+            // 
+            // DBA_SYS_PRIVS_dataGridView
+            // 
+            DBA_SYS_PRIVS_dataGridView.AllowUserToAddRows = false;
+            DBA_SYS_PRIVS_dataGridView.AllowUserToDeleteRows = false;
+            DBA_SYS_PRIVS_dataGridView.BackgroundColor = Color.White;
+            DBA_SYS_PRIVS_dataGridView.BorderStyle = BorderStyle.Fixed3D;
+            DBA_SYS_PRIVS_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DBA_SYS_PRIVS_dataGridView.GridColor = Color.BurlyWood;
+            DBA_SYS_PRIVS_dataGridView.Location = new Point(1000, 212);
+            DBA_SYS_PRIVS_dataGridView.Name = "DBA_SYS_PRIVS_dataGridView";
+            DBA_SYS_PRIVS_dataGridView.ReadOnly = true;
+            DBA_SYS_PRIVS_dataGridView.RowHeadersWidth = 82;
+            DBA_SYS_PRIVS_dataGridView.Size = new Size(608, 306);
+            DBA_SYS_PRIVS_dataGridView.TabIndex = 16;
+            // 
+            // DBA_TAB_PRIVS_dataGridView
+            // 
+            DBA_TAB_PRIVS_dataGridView.AllowUserToAddRows = false;
+            DBA_TAB_PRIVS_dataGridView.AllowUserToDeleteRows = false;
+            DBA_TAB_PRIVS_dataGridView.BackgroundColor = Color.White;
+            DBA_TAB_PRIVS_dataGridView.BorderStyle = BorderStyle.Fixed3D;
+            DBA_TAB_PRIVS_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DBA_TAB_PRIVS_dataGridView.GridColor = Color.BurlyWood;
+            DBA_TAB_PRIVS_dataGridView.Location = new Point(1000, 524);
+            DBA_TAB_PRIVS_dataGridView.Name = "DBA_TAB_PRIVS_dataGridView";
+            DBA_TAB_PRIVS_dataGridView.ReadOnly = true;
+            DBA_TAB_PRIVS_dataGridView.RowHeadersWidth = 82;
+            DBA_TAB_PRIVS_dataGridView.Size = new Size(608, 244);
+            DBA_TAB_PRIVS_dataGridView.TabIndex = 17;
+            // 
+            // DBA_ROLE_PRIVS_dataGridView
+            // 
+            DBA_ROLE_PRIVS_dataGridView.AllowUserToAddRows = false;
+            DBA_ROLE_PRIVS_dataGridView.AllowUserToDeleteRows = false;
+            DBA_ROLE_PRIVS_dataGridView.BackgroundColor = Color.White;
+            DBA_ROLE_PRIVS_dataGridView.BorderStyle = BorderStyle.Fixed3D;
+            DBA_ROLE_PRIVS_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DBA_ROLE_PRIVS_dataGridView.GridColor = Color.BurlyWood;
+            DBA_ROLE_PRIVS_dataGridView.Location = new Point(1000, 774);
+            DBA_ROLE_PRIVS_dataGridView.Name = "DBA_ROLE_PRIVS_dataGridView";
+            DBA_ROLE_PRIVS_dataGridView.ReadOnly = true;
+            DBA_ROLE_PRIVS_dataGridView.RowHeadersWidth = 82;
+            DBA_ROLE_PRIVS_dataGridView.Size = new Size(608, 262);
+            DBA_ROLE_PRIVS_dataGridView.TabIndex = 18;
+            // 
+            // delete_button
+            // 
+            delete_button.BackColor = SystemColors.ActiveBorder;
+            delete_button.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            delete_button.ForeColor = Color.Transparent;
+            delete_button.Location = new Point(503, 1068);
+            delete_button.Name = "delete_button";
+            delete_button.Size = new Size(322, 78);
+            delete_button.TabIndex = 40;
+            delete_button.Text = "Xóa user";
+            delete_button.UseVisualStyleBackColor = false;
+            // 
+            // edit_button
+            // 
+            edit_button.BackColor = Color.BurlyWood;
+            edit_button.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            edit_button.ForeColor = Color.Transparent;
+            edit_button.Location = new Point(845, 1068);
+            edit_button.Name = "edit_button";
+            edit_button.Size = new Size(322, 78);
+            edit_button.TabIndex = 39;
+            edit_button.Text = "Sửa thông tin";
+            edit_button.UseVisualStyleBackColor = false;
+            // 
+            // search_employee_button
+            // 
+            search_employee_button.BackColor = Color.BurlyWood;
+            search_employee_button.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            search_employee_button.ForeColor = Color.Transparent;
+            search_employee_button.Location = new Point(592, 130);
+            search_employee_button.Name = "search_employee_button";
+            search_employee_button.Size = new Size(149, 63);
+            search_employee_button.TabIndex = 45;
+            search_employee_button.Text = "Tra cứu";
+            search_employee_button.UseVisualStyleBackColor = false;
+            // 
+            // add_button
+            // 
+            add_button.BackColor = Color.BurlyWood;
+            add_button.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            add_button.ForeColor = Color.Transparent;
+            add_button.Location = new Point(1310, 122);
+            add_button.Name = "add_button";
+            add_button.Size = new Size(298, 78);
+            add_button.TabIndex = 44;
+            add_button.Text = "Thêm user";
+            add_button.UseVisualStyleBackColor = false;
+            // 
+            // search_employee_guna2TextBox
+            // 
+            search_employee_guna2TextBox.BorderRadius = 20;
+            search_employee_guna2TextBox.CustomizableEdges = customizableEdges1;
+            search_employee_guna2TextBox.DefaultText = "";
+            search_employee_guna2TextBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            search_employee_guna2TextBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            search_employee_guna2TextBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            search_employee_guna2TextBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            search_employee_guna2TextBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            search_employee_guna2TextBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            search_employee_guna2TextBox.HoverState.BorderColor = Color.Gray;
+            search_employee_guna2TextBox.IconLeft = Properties.Resources.search;
+            search_employee_guna2TextBox.IconLeftOffset = new Point(10, 0);
+            search_employee_guna2TextBox.IconRight = Properties.Resources.close__1_;
+            search_employee_guna2TextBox.IconRightOffset = new Point(10, 0);
+            search_employee_guna2TextBox.Location = new Point(222, 138);
+            search_employee_guna2TextBox.Margin = new Padding(7);
+            search_employee_guna2TextBox.Name = "search_employee_guna2TextBox";
+            search_employee_guna2TextBox.PlaceholderText = "";
+            search_employee_guna2TextBox.SelectedText = "";
+            search_employee_guna2TextBox.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            search_employee_guna2TextBox.Size = new Size(360, 51);
+            search_employee_guna2TextBox.TabIndex = 43;
+            // 
+            // search_employee_label
+            // 
+            search_employee_label.AutoSize = true;
+            search_employee_label.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            search_employee_label.Location = new Point(25, 142);
+            search_employee_label.Margin = new Padding(0);
+            search_employee_label.Name = "search_employee_label";
+            search_employee_label.Size = new Size(190, 37);
+            search_employee_label.TabIndex = 42;
+            search_employee_label.Text = "Mã nhân viên:";
+            // 
             // Admin_QLUsers
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
+            Controls.Add(search_employee_button);
+            Controls.Add(add_button);
+            Controls.Add(search_employee_guna2TextBox);
+            Controls.Add(search_employee_label);
+            Controls.Add(delete_button);
+            Controls.Add(edit_button);
+            Controls.Add(DBA_ROLE_PRIVS_dataGridView);
+            Controls.Add(DBA_TAB_PRIVS_dataGridView);
+            Controls.Add(DBA_SYS_PRIVS_dataGridView);
+            Controls.Add(dataGridView);
             Controls.Add(guna2Separator1);
             Controls.Add(label1);
             Margin = new Padding(0);
             Name = "Admin_QLUsers";
             Size = new Size(1625, 1169);
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DBA_SYS_PRIVS_dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DBA_TAB_PRIVS_dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DBA_ROLE_PRIVS_dataGridView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -70,5 +244,15 @@
 
         private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
         private Label label1;
+        private DataGridView dataGridView;
+        private DataGridView DBA_SYS_PRIVS_dataGridView;
+        private DataGridView DBA_TAB_PRIVS_dataGridView;
+        private DataGridView DBA_ROLE_PRIVS_dataGridView;
+        private Button delete_button;
+        private Button edit_button;
+        private Button search_employee_button;
+        private Button add_button;
+        private Guna.UI2.WinForms.Guna2TextBox search_employee_guna2TextBox;
+        private Label search_employee_label;
     }
 }
---
+++ QLTruongDH/Admin_ThemUser.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace QLTruongDH
+{
+    partial class Admin_ThemUser
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            reset_button = new Button();
+            add_button = new Button();
+            guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            label1 = new Label();
+            username_textBox = new TextBox();
+            username_label = new Label();
+            username_panel = new Panel();
+            password_panel = new Panel();
+            password_label = new Label();
+            password_textBox = new TextBox();
+            sys_priv_label = new Label();
+            add_user_sys_checkedListBox = new CheckedListBox();
+            role_priv_label = new Label();
+            add_user_role_checkedListBox = new CheckedListBox();
+            tab_priv_label = new Label();
+            add_user_select_table_comboBox = new ComboBox();
+            add_user_tab_checkedListBox = new CheckedListBox();
+            add_user_with_grant_option_checkBox = new CheckBox();
+            add_user_column_checkedListBox = new CheckedListBox();
+            checkBox1 = new CheckBox();
+            username_panel.SuspendLayout();
+            password_panel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // reset_button
+            // 
+            reset_button.BackColor = SystemColors.ActiveBorder;
+            reset_button.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            reset_button.ForeColor = Color.Transparent;
+            reset_button.Location = new Point(504, 1033);
+            reset_button.Name = "reset_button";
+            reset_button.Size = new Size(322, 78);
+            reset_button.TabIndex = 44;
+            reset_button.Text = "Nhập lại";
+            reset_button.UseVisualStyleBackColor = false;
+            // 
+            // add_button
+            // 
+            add_button.BackColor = Color.BurlyWood;
+            add_button.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            add_button.ForeColor = Color.Transparent;
+            add_button.Location = new Point(846, 1033);
+            add_button.Name = "add_button";
+            add_button.Size = new Size(322, 78);
+            add_button.TabIndex = 43;
+            add_button.Text = "Thêm user";
+            add_button.UseVisualStyleBackColor = false;
+            // 
+            // guna2Separator1
+            // 
+            guna2Separator1.FillThickness = 2;
+            guna2Separator1.Location = new Point(0, 100);
+            guna2Separator1.Margin = new Padding(0);
+            guna2Separator1.Name = "guna2Separator1";
+            guna2Separator1.Size = new Size(1620, 21);
+            guna2Separator1.TabIndex = 42;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(25, 25);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(235, 59);
+            label1.TabIndex = 41;
+            label1.Text = "Thêm user";
+            // 
+            // username_textBox
+            // 
+            username_textBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            username_textBox.Location = new Point(248, 19);
+            username_textBox.Name = "username_textBox";
+            username_textBox.Size = new Size(297, 43);
+            username_textBox.TabIndex = 46;
+            // 
+            // username_label
+            // 
+            username_label.AutoSize = true;
+            username_label.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            username_label.Location = new Point(0, 19);
+            username_label.Margin = new Padding(0);
+            username_label.Name = "username_label";
+            username_label.Size = new Size(204, 37);
+            username_label.TabIndex = 45;
+            username_label.Text = "Tên đăng nhập:";
+            // 
+            // username_panel
+            // 
+            username_panel.Controls.Add(username_label);
+            username_panel.Controls.Add(username_textBox);
+            username_panel.Location = new Point(25, 121);
+            username_panel.Margin = new Padding(0);
+            username_panel.Name = "username_panel";
+            username_panel.Size = new Size(554, 69);
+            username_panel.TabIndex = 47;
+            // 
+            // password_panel
+            // 
+            password_panel.Controls.Add(password_label);
+            password_panel.Controls.Add(password_textBox);
+            password_panel.Location = new Point(650, 121);
+            password_panel.Name = "password_panel";
+            password_panel.Size = new Size(554, 69);
+            password_panel.TabIndex = 48;
+            // 
+            // password_label
+            // 
+            password_label.AutoSize = true;
+            password_label.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            password_label.Location = new Point(0, 19);
+            password_label.Margin = new Padding(0);
+            password_label.Name = "password_label";
+            password_label.Size = new Size(140, 37);
+            password_label.TabIndex = 45;
+            password_label.Text = "Mật khẩu:";
+            // 
+            // password_textBox
+            // 
+            password_textBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            password_textBox.Location = new Point(171, 19);
+            password_textBox.Name = "password_textBox";
+            password_textBox.Size = new Size(374, 43);
+            password_textBox.TabIndex = 46;
+            // 
+            // sys_priv_label
+            // 
+            sys_priv_label.AutoSize = true;
+            sys_priv_label.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            sys_priv_label.Location = new Point(25, 233);
+            sys_priv_label.Name = "sys_priv_label";
+            sys_priv_label.Size = new Size(176, 37);
+            sys_priv_label.TabIndex = 49;
+            sys_priv_label.Text = "Sys privilege:";
+            // 
+            // add_user_sys_checkedListBox
+            // 
+            add_user_sys_checkedListBox.BorderStyle = BorderStyle.None;
+            add_user_sys_checkedListBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            add_user_sys_checkedListBox.FormattingEnabled = true;
+            add_user_sys_checkedListBox.Items.AddRange(new object[] { "Connect", "Create session", "Create/drop/alter user", "Create/drop/alter function", "Create/drop/alter procedure", "Create/drop/alter table", "Create/drop/alter view" });
+            add_user_sys_checkedListBox.Location = new Point(25, 287);
+            add_user_sys_checkedListBox.Name = "add_user_sys_checkedListBox";
+            add_user_sys_checkedListBox.Size = new Size(394, 280);
+            add_user_sys_checkedListBox.TabIndex = 50;
+            // 
+            // role_priv_label
+            // 
+            role_priv_label.AutoSize = true;
+            role_priv_label.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            role_priv_label.Location = new Point(650, 233);
+            role_priv_label.Name = "role_priv_label";
+            role_priv_label.Size = new Size(189, 37);
+            role_priv_label.TabIndex = 51;
+            role_priv_label.Text = "Role privilege:";
+            // 
+            // add_user_role_checkedListBox
+            // 
+            add_user_role_checkedListBox.BorderStyle = BorderStyle.None;
+            add_user_role_checkedListBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            add_user_role_checkedListBox.FormattingEnabled = true;
+            add_user_role_checkedListBox.Location = new Point(650, 287);
+            add_user_role_checkedListBox.Name = "add_user_role_checkedListBox";
+            add_user_role_checkedListBox.Size = new Size(493, 280);
+            add_user_role_checkedListBox.TabIndex = 52;
+            // 
+            // tab_priv_label
+            // 
+            tab_priv_label.AutoSize = true;
+            tab_priv_label.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tab_priv_label.Location = new Point(25, 620);
+            tab_priv_label.Name = "tab_priv_label";
+            tab_priv_label.Size = new Size(178, 37);
+            tab_priv_label.TabIndex = 53;
+            tab_priv_label.Text = "Tab privilege:";
+            // 
+            // add_user_select_table_comboBox
+            // 
+            add_user_select_table_comboBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            add_user_select_table_comboBox.FormattingEnabled = true;
+            add_user_select_table_comboBox.Location = new Point(25, 675);
+            add_user_select_table_comboBox.Name = "add_user_select_table_comboBox";
+            add_user_select_table_comboBox.Size = new Size(339, 45);
+            add_user_select_table_comboBox.TabIndex = 54;
+            add_user_select_table_comboBox.Text = "Chọn bảng";
+            // 
+            // add_user_tab_checkedListBox
+            // 
+            add_user_tab_checkedListBox.BorderStyle = BorderStyle.None;
+            add_user_tab_checkedListBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            add_user_tab_checkedListBox.FormattingEnabled = true;
+            add_user_tab_checkedListBox.Items.AddRange(new object[] { "SELECT", "INSERT", "DELETE", "UPDATE" });
+            add_user_tab_checkedListBox.Location = new Point(25, 746);
+            add_user_tab_checkedListBox.Name = "add_user_tab_checkedListBox";
+            add_user_tab_checkedListBox.Size = new Size(178, 160);
+            add_user_tab_checkedListBox.TabIndex = 55;
+            // 
+            // add_user_with_grant_option_checkBox
+            // 
+            add_user_with_grant_option_checkBox.AutoSize = true;
+            add_user_with_grant_option_checkBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            add_user_with_grant_option_checkBox.ForeColor = Color.Goldenrod;
+            add_user_with_grant_option_checkBox.Location = new Point(209, 746);
+            add_user_with_grant_option_checkBox.Name = "add_user_with_grant_option_checkBox";
+            add_user_with_grant_option_checkBox.Size = new Size(310, 41);
+            add_user_with_grant_option_checkBox.TabIndex = 56;
+            add_user_with_grant_option_checkBox.Text = "WITH GRANT OPTION";
+            add_user_with_grant_option_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // add_user_column_checkedListBox
+            // 
+            add_user_column_checkedListBox.BorderStyle = BorderStyle.None;
+            add_user_column_checkedListBox.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            add_user_column_checkedListBox.FormattingEnabled = true;
+            add_user_column_checkedListBox.Location = new Point(650, 620);
+            add_user_column_checkedListBox.Name = "add_user_column_checkedListBox";
+            add_user_column_checkedListBox.Size = new Size(493, 360);
+            add_user_column_checkedListBox.TabIndex = 57;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox1.ForeColor = Color.Goldenrod;
+            checkBox1.Location = new Point(209, 793);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(310, 41);
+            checkBox1.TabIndex = 58;
+            checkBox1.Text = "WITH GRANT OPTION";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Admin_ThemUser
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(checkBox1);
+            Controls.Add(add_user_column_checkedListBox);
+            Controls.Add(add_user_with_grant_option_checkBox);
+            Controls.Add(add_user_tab_checkedListBox);
+            Controls.Add(add_user_select_table_comboBox);
+            Controls.Add(tab_priv_label);
+            Controls.Add(add_user_role_checkedListBox);
+            Controls.Add(role_priv_label);
+            Controls.Add(add_user_sys_checkedListBox);
+            Controls.Add(sys_priv_label);
+            Controls.Add(password_panel);
+            Controls.Add(username_panel);
+            Controls.Add(reset_button);
+            Controls.Add(add_button);
+            Controls.Add(guna2Separator1);
+            Controls.Add(label1);
+            Margin = new Padding(0);
+            Name = "Admin_ThemUser";
+            Size = new Size(1625, 1169);
+            username_panel.ResumeLayout(false);
+            username_panel.PerformLayout();
+            password_panel.ResumeLayout(false);
+            password_panel.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button reset_button;
+        private Button add_button;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private Label label1;
+        private TextBox username_textBox;
+        private Label username_label;
+        private Panel username_panel;
+        private Panel password_panel;
+        private Label password_label;
+        private TextBox password_textBox;
+        private Label sys_priv_label;
+        private CheckedListBox add_user_sys_checkedListBox;
+        private Label role_priv_label;
+        private CheckedListBox add_user_role_checkedListBox;
+        private Label tab_priv_label;
+        private ComboBox add_user_select_table_comboBox;
+        private CheckedListBox add_user_tab_checkedListBox;
+        private CheckBox add_user_with_grant_option_checkBox;
+        private CheckedListBox add_user_column_checkedListBox;
+        private CheckBox checkBox1;
+    }
+}
